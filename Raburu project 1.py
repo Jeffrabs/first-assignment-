e4891fccacae5796031f6622b1588890052d5a9a
Jeffrabs <jffrabs@gmail.com>
2024-06-30 19:26:27 +0300

    Add files via upload
    first assignment

---
+++ Raburu project 1.py
@@ -0,0 +1,20 @@
+student_name= "Geoffrey_Raburu"   #name of the student
+
+math=75
+science=80
+english=90     #subject taken by the student
+
+average=(math+science+english)/3
+print(average)                      #average of the 3 subjects
+
+Mylist=[math,science,english]
+print(Mylist)
+
+mydict= {
+"student_name": "Geoffrey_Raburu",
+"math":75,
+"science":80,
+"english":90,
+}
+print(mydict)
+
